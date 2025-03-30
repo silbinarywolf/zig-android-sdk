84ac82c12328165b99bbf9f345e459f54d20c614
Jae B <doogie1012@gmail.com>
2025-03-30 12:02:49 +1100

    more work

--- examples/sdl2/android/androidCrashTest/ZigSDLActivity.java
+++ examples/sdl2/android/androidCrashTest/ZigSDLActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.util.AndroidRuntimeException;
 
 /**
- * A sample wrapper class that just calls SDLActivity
+ * Used by ci.yml to make the application crash if an error occurs initializing your application.
  * 
- * Used for testing only to detect if it crashes
+ * This allows the following commands to catch crash errors on startup:
+ * - adb install ./zig-out/bin/sdl-zig-demo.apk
+ * - adb shell monkey --kill-process-after-error --monitor-native-crashes --pct-touch 100 -p com.zig.sdl2 -v 50
  */
 public class ZigSDLActivity extends SDLActivity {
     @Override
